f4ef0d7d83222addb40b8023ae685856279dfce5
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-15 15:50:29 +0200

    added function to check if a character is a number

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const passwdInputEl = document.getElementById('password-input');
+const specialChars = ["'",'`',"!","@","#","$","%","^","&","*","(",")","_","+","-","=","[","]","{","}",";","\"",":","|",".","<",">","\\","/","?","~"];
 
 passwdInputEl.addEventListener('selectionchange', () => {
+    // console.log(passwdInputEl.value)
     checkStringLength(passwdInputEl.value, 9, document.querySelector('.requirements > .no_char'));
-    checkUpperCase(passwdInputEl.value, document.querySelector('.requirements > .uppercase'))
+    checkIfUpperCase(passwdInputEl.value, document.querySelector('.requirements > .uppercase'))
+    checkIfNumber(passwdInputEl.value, document.querySelector('.requirements > .number'))
 })
 
 // --> function to check the input value length
@@ -26,13 +29,13 @@ function checkStringLength(value, minlength, requirementEl){
  * @param {*} value  the input value whose characters needs to be checked
  * @param {*} requirementEl the element that show the requirement to meet
  */
-function checkUpperCase(value, requirementEl){
+function checkIfUpperCase(value, requirementEl){
     value = value.trim();
     let char;
     let isUpperCase = false;
     for(let i= 0; i < value.length; i++){
         char = value.charAt(i);
-        if (char === char.toUpperCase()){
+        if (char === char.toUpperCase() && !Number.isInteger(Number.parseInt(char, 10)) && !specialChars.includes(char)){
             isUpperCase = true;
         }
     }
@@ -40,6 +43,31 @@ function checkUpperCase(value, requirementEl){
     if(isUpperCase === true && !requirementEl.classList.contains('checked')){
         requirementEl.classList.add('checked');
     } else if(isUpperCase === false && requirementEl.classList.contains('checked')){
+        requirementEl.classList.remove('checked');
+    }
+
+}
+
+// --> function to check if a character is a number
+/**
+ * 
+ * @param {*} value  the input value whose characters needs to be checked
+ * @param {*} requirementEl the element that show the requirement to meet
+ */
+function checkIfNumber(value, requirementEl){
+    value = value.trim();
+    let char;
+    let isNumber = false;
+    for(let i= 0; i < value.length; i++){
+        char = value.charAt(i);
+        if (Number.isInteger(Number.parseInt(char, 10))){
+            isNumber = true;
+        }
+    }
+
+    if(isNumber === true && !requirementEl.classList.contains('checked')){
+        requirementEl.classList.add('checked');
+    } else if(isNumber === false && requirementEl.classList.contains('checked')){
         requirementEl.classList.remove('checked');
     }
 
