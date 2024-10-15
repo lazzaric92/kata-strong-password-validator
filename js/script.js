3ff67112000e3a1f093fd57cb2e29adcbb6308d9
lazzaric92 <lazzaric_92@hotmail.it>
2024-10-15 12:56:45 +0200

    added function to check if a character is uppercase

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const passwdInputEl = document.getElementById('password-input');
 
 passwdInputEl.addEventListener('selectionchange', () => {
     checkStringLength(passwdInputEl.value, 9, document.querySelector('.requirements > .no_char'));
+    checkUpperCase(passwdInputEl.value, document.querySelector('.requirements > .uppercase'))
 })
 
 // --> function to check the input value length
@@ -9,7 +10,7 @@ passwdInputEl.addEventListener('selectionchange', () => {
  * 
  * @param {*} value  the input value whose length needs to be checked
  * @param {*} minlength the minimum number of characters required
- * @param {*} requirementEl if passed, the element that show the requirement to meet
+ * @param {*} requirementEl the element that show the requirement to meet
  */
 function checkStringLength(value, minlength, requirementEl){
     if(value.trim().length >= minlength && !requirementEl.classList.contains('checked')){
@@ -17,4 +18,29 @@ function checkStringLength(value, minlength, requirementEl){
     } else if(value.trim().length < minlength && requirementEl.classList.contains('checked')){
         requirementEl.classList.remove('checked');
     }
+}
+
+// --> function to check if a character is uppercase
+/**
+ * 
+ * @param {*} value  the input value whose characters needs to be checked
+ * @param {*} requirementEl the element that show the requirement to meet
+ */
+function checkUpperCase(value, requirementEl){
+    value = value.trim();
+    let char;
+    let isUpperCase = false;
+    for(let i= 0; i < value.length; i++){
+        char = value.charAt(i);
+        if (char === char.toUpperCase()){
+            isUpperCase = true;
+        }
+    }
+
+    if(isUpperCase === true && !requirementEl.classList.contains('checked')){
+        requirementEl.classList.add('checked');
+    } else if(isUpperCase === false && requirementEl.classList.contains('checked')){
+        requirementEl.classList.remove('checked');
+    }
+
 }
